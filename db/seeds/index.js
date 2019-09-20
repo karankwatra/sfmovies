438f6732833292d1c32077771071ffa29e9e8145
Karan Kwatra <karan@lob.com>
2019-09-20 10:28:09 -0700

    feat(locations): (1/4) db migrations to support locations (#10)

--- db/seeds/index.js
+++ db/seeds/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
-const Movies = require('./data/movies');
+const Movies    = require('./data/movies');
+const Locations = require('./data/locations');
 
 exports.seed = async (Knex) => {
-  await Knex('movies').truncate();
+  await Knex.raw('TRUNCATE movies CASCADE;');
   await Knex('movies').insert(Movies);
+  await Knex.raw('TRUNCATE locations CASCADE');
+  await Knex('locations').insert(Locations);
 };
