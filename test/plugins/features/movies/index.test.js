5b2cc67418d8d12e4e3c20065be1fec0a2b9060b
Karan Kwatra <karan@lob.com>
2019-09-24 17:06:37 -0700

    fix(locations): added more beforeeach

--- test/plugins/features/movies/index.test.js
+++ test/plugins/features/movies/index.test.js
@@ -1,10 +1,16 @@
 'use strict';
 
+const Knex = require('../../../../lib/libraries/knex');
+
 const Movies = require('../../../../lib/server');
 
 describe('movies integration', () => {
 
   describe('create', () => {
+
+    beforeEach(async () => {
+      await Knex.raw('TRUNCATE movies CASCADE; TRUNCATE locations CASCADE; TRUNCATE locations_movies CASCADE;');
+    });
 
     it('creates a movie', async () => {
       const response = await Movies.inject({
@@ -21,6 +27,10 @@ describe('movies integration', () => {
   });
 
   describe('retrieve', () => {
+
+    beforeEach(async () => {
+      await Knex.raw('TRUNCATE movies CASCADE; TRUNCATE locations CASCADE; TRUNCATE locations_movies CASCADE;');
+    });
 
     it('retrieves a movie', async () => {
       const response = await Movies.inject({
