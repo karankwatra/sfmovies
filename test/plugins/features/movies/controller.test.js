43bda2719d5608fe003c4d3bc361c334250da113
Karan Kwatra <karan@lob.com>
2019-09-26 16:55:05 -0700

    feat(locations): (3/4) add POST endpoint to add new locations to a movie (#12)

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -10,6 +10,7 @@ const MovieFactory         = require('../../../factories/movie');
 
 const testMovie1 = MovieFactory.build({ name: 'Zodiac', release_year: 2007 });
 const testMovie2 = MovieFactory.build({ name: 'Dawn of the Planet of the Apes', release_year: 2014 });
+const testMovie3 = MovieFactory.build({ name: 'Ant-Man', release_year: 2015 });
 
 const testLocation = LocationFactory.build();
 const testLocMov   = LocationMovieFactory.build({ movie_id: testMovie1.id, location_id: testLocation.id });
@@ -18,9 +19,8 @@ describe('movie controller', () => {
 
   beforeEach(async () => {
     await Knex.raw('TRUNCATE movies CASCADE; TRUNCATE locations CASCADE; TRUNCATE locations_movies CASCADE;');
-    await Knex('movies').insert([testMovie1, testMovie2]);
+    await Knex('movies').insert([testMovie1, testMovie2, testMovie3]);
     await Knex('locations').insert(testLocation);
-    await Knex('locations_movies').insert(testLocMov);
   });
 
   describe('create', () => {
@@ -39,7 +39,7 @@ describe('movie controller', () => {
       const query = {};
       const movies = await Controller.list(query);
 
-      expect(movies.length).to.eql(2);
+      expect(movies.length).to.eql(3);
     });
 
     it('retrieves movies from a specific year', async () => {
@@ -82,15 +82,59 @@ describe('movie controller', () => {
 
   describe('retrieve movies with locations', () => {
 
+    beforeEach(async () => {
+      await Knex('locations_movies').insert(testLocMov);
+    });
+
     it('retrieves a movie with the related locations', async () => {
       const query = {};
       const movies = await Controller.list(query);
 
-      expect(movies.length).to.eql(2);
+      expect(movies.length).to.eql(3);
       expect(movies.models[0].get('name')).to.eql(testMovie1.name);
       expect(movies.models[0].relations.locations.models[0].attributes.name).to.eql(testLocation.name);
     });
 
+  });
+
+  describe('add location to movie', () => {
+
+    it('adds a location to a movie', async () => {
+      const payload = { locationName: testLocation.name };
+      const movie = await Controller.addLocationToMovie(testMovie2.id, payload);
+
+      expect(movie.relations.locations.length).to.eql(1);
+      expect(movie.relations.locations.models[0].get('name')).to.eql(payload.locationName);
+    });
+
+    it('does not add a location to a movie twice', async () => {
+      const payload = { locationName: testLocation.name };
+
+      await Controller.addLocationToMovie(testMovie2.id, payload);
+
+      const movie = await Controller.addLocationToMovie(testMovie2.id, payload);
+
+      expect(movie.relations.locations.length).to.eql(1);
+      expect(movie.relations.locations.models[0].get('name')).to.eql(payload.locationName);
+    });
+
+    it('adds a new location to a movie', async () => {
+      const payload = { locationName: 'San Francisco' };
+      const movie = await Controller.addLocationToMovie(testMovie3.id, payload);
+
+      expect(movie.relations.locations.length).to.eql(1);
+      expect(movie.relations.locations.models[0].get('name')).to.eql(payload.locationName);
+    });
+
+  });
+
+  it('errs when adding a location to a non-existent movie', async () => {
+    const payload = { locationName: testLocation.name };
+
+    await Controller.addLocationToMovie(9999, payload)
+    .catch((err) => {
+      expect(err.output.statusCode).to.eql(404);
+    });
   });
 
 });
